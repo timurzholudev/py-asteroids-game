8f47fff5b2840f01897426f7fe5863ebe0f86b01
timurzholudev <timurzholudev@gmail.com>
2025-10-18 21:44:08 +0200

    feat: add player functionality

--- main.py
+++ main.py
@@ -6,7 +6,14 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     dt = 0
 
     while True:
@@ -14,8 +21,12 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
+        updatable.update(dt)
+
         screen.fill("black")
-        player.draw(screen)
+        for obj in updatable:
+            obj.draw(screen)
+
         pygame.display.flip()
 
         # Limit to 60 FPS
